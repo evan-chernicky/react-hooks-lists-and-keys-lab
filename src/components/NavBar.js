afcdea911024dfcd8e64cd48703e01a7f5bc97dd
Evan Chernicky <echernicky@gmail.com>
2022-02-25 01:34:20 -0500

    commit tree c98a1c453f7ec646a70466277215a91844c0bb04

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const linkLinks = links.map(link => <a key={link} href={'#' + link}>{link}</a>)
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  
+
+  return <nav>{linkLinks}</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,23 @@
 import React from "react";
 
-function ProjectItem({ name, about, technologies }) {
+function ProjectItem({name, about, technologies }) {
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+      {renderTechnologies(technologies)}
       </div>
     </div>
   );
+}
+
+function renderTechnologies(technologies) {
+
+  const technology = technologies.map(technology => <span key={technology}>{technology}</span> )
+
+  return technology
+
 }
 
 export default ProjectItem;
